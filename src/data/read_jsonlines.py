237b6ac423fa3f0bb6a5e9297a1e06f26465d45d
Javier Daza <javierdazacomas@gmail.com>
2023-10-19 17:12:28 -0500

    refactor: Aclarar proposito de archivo read_jsonlines.py

--- src/data/read_jsonlines.py
+++ src/data/read_jsonlines.py
@@ -1,12 +1,18 @@
+'''
+Archivo de prueba:
+
+- Probar la herramienta petl
+- Leer los datos extraidos de Scrappy en formato jsonlines (/data/raw/items.jsonl)
+'''
+
 import petl as etl
 
 datos = etl.fromjson('items.jsonl', lines=True)
 
-# <class 'petl.io.json.JsonView'>
-# print('La variable datos es de tipo: ', type(datos))
+# print('La variable datos es de tipo: ', type(datos))  # <class 'petl.io.json.JsonView'>
 # print('El lenght de los datos: ', len(datos))  # 856,383
 
-#print(datos[0])  # ('title', 'url', 'date', 'category')
+# print(datos[0])  # ('title', 'url', 'date', 'category')
 
 print(
 	'Revisar la cantidad de visitas de cada url para luego,'
@@ -15,4 +21,3 @@ print(
 print(datos[1][1], end='\n\n')  # url del item 1
 print(datos[2][1], end='\n\n')
 print(datos[3][1], end='\n\n')
-
